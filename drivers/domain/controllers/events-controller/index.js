9ca48f8775ab83dd99a434bc2692cfa2d947073d
Nejat Mhango <nejatmhango@gmail.com>
2020-02-09 18:54:13 +0200

    Added moment.js

--- drivers/domain/controllers/events-controller/index.js
+++ drivers/domain/controllers/events-controller/index.js
@@ -6,12 +6,20 @@ const private = {};
 
 public.getAllEvents = async (req, res) => {
   try {
-    const result = await eventsDb.find({id: '1933'}, 'name');
+    const result = await eventsDb.find();
     res.json({result});
   } catch (e) {
     throw new Error(e).message;
   }
 };
+
+public.createEvent = async (req, res) => {
+  try {
+    console.log(req.body);
+  } catch (e) {
+    res.status(400).send(`'Bad Request:' ${new Error(e).message}`);
+  }
+}
 
 
 const EventsController = public;
